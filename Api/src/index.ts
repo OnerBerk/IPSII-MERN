ce97a3ad987894722c1787c5e07964d687a7908e
OnerBerk <oner.berk@joza-it.fr>
2023-10-13 17:30:59 +0200

    express

--- Api/src/index.ts
+++ Api/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Welcome to project MERN")
+const express = require('express')
+const dotenv = require('dotenv')
+import {Request, Response} from "express";
+
+const app = express()
+const port: string | number = process.env.PORT || 8080
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to Mern')
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})

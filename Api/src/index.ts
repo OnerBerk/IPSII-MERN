aab3a14c4ec4b3b2ec6dd032fb120c85da921743
OnerBerk <oner.berk@joza-it.fr>
2023-10-14 15:10:22 +0200

    add first route

--- Api/src/index.ts
+++ Api/src/index.ts
@@ -1,14 +1,20 @@
+import {Student} from "./schemas/student.schema";
+
 const express = require('express')
 require('dotenv').config();
 import {Request, Response} from "express";
-import {json} from 'body-parser'
 import {connectDB} from "./config/db"
 
 const app = express()
 const port = process.env.PORT || 8080
-connectDB();
+connectDB().catch((e) => console.log({error: e}));
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to Mern')
+})
+
+app.get('/students', async (req: Request, res: Response) => {
+    const students = await Student.find();
+    res.json({students: students})
 })
 
 app.listen(port, () => {
---
+++ Api/src/schemas/student.schema.ts
@@ -0,0 +1,9 @@
+import mongoose ,{Schema,model}from "mongoose";
+import {IStudent} from "../domain/student.interface";
+
+const StudentSchema = new Schema<IStudent>({
+    name: {type:String,required:true},
+    age: Number,
+    email: {type: String, unique: true}
+})
+export const Student=model<IStudent>('student', StudentSchema,'students')
